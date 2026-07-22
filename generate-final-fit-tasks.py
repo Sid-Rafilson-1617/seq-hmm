0f905ce33f687b7726e73163613c85aefca13aff
Sid Rafilson <srafilson@worker6019.cm.cluster>
2026-07-22 15:16:03 -0400

    co-smooth-pf

--- generate-final-fit-tasks.py
+++ generate-final-fit-tasks.py
@@ -5,18 +5,18 @@ task_file = Path("tasks/hmm_tasks.txt")
 task_file.parent.mkdir(exist_ok=True)
 
 data_dir = '/mnt/home/srafilson/code/sequences/data'
-data = f"{data_dir}/maze_clipped_spike_counts_Bilat_R02_20251106.npz"
+data = f"{data_dir}/maze_clipped_spike_counts_pyr_only_Bilat_R02_20251106.npz"
 
-save_dir = "results_all_cells_20ms/full_fit"
+save_dir = "results_pyr_only_20ms/full_fit"
 Path(save_dir).mkdir(parents=True, exist_ok=True)
 
 
 nStates = [251]
 
-#restarts = range(2000)
-start = 438
-end = 2000
-restarts = np.arange(start, end)
+restarts = range(1000)
+#start = 438
+#end = 2000
+#restarts = np.arange(start, end)
 
 
 with task_file.open("w") as f:
@@ -26,7 +26,7 @@ with task_file.open("w") as f:
 
             cmd = (
                 "python -m fit_one_full "
-                f"--model-params {data} "
+                f"--data {data} "
                 f"--save-dir {save_dir} "
                 f"--nStates {N} "
                 f"--restart {restart} "
